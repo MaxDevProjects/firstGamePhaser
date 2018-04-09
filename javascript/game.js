cfdbf45783692b84ab6c31983a8471ab63aec418
MaxDevProjects <proad.maxime.martin@gmail.com>
2018-04-09 17:18:58 +0200

    responsive

--- javascript/game.js
+++ javascript/game.js
@@ -13,9 +13,21 @@ function create() {
     if (!game.device.desktop) {     
         game.input.onDown.add(gofull, this); 
     }
+    this.onresize();
 }
 function update() {
     
+}
+function onResize() {
+    // this function is called each time the browser is resized, and re-positions
+    // game elements to keep them in their right position according to game size
+    levelText.x = Math.round((game.width - levelText.width) / 2);
+    levelText.y = game.height;
+    titleText.x = Math.round((game.width - titleText.width) / 2);
+    fixedGroup.x = Math.round((game.width - 320) / 2);
+    fixedGroup.y = Math.round((game.height - 320) / 2);
+    movingGroup.x = Math.round((game.width - 320) / 2);
+    movingGroup.y = Math.round((game.height - 320) / 2);
 }
 function render() {
     
